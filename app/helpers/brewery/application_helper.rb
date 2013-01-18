90342dc12f41fa04ff159026769667c4cb286f42
Nathan Samson <nathansamson@gmail.com>
2013-01-18 01:04:37 +0100

    Use CSS loading icon instead of CSS image.

--- app/helpers/brewery/application_helper.rb
+++ app/helpers/brewery/application_helper.rb
@@ -1,4 +1,11 @@
 module Brewery
   module ApplicationHelper
+    def css_loading_icon
+      return '<div class="bubblingG">
+          <span id="bubblingG_1"></span>
+          <span id="bubblingG_2"></span>
+          <span id="bubblingG_3"></span>
+        </div>'.html_safe
+    end
   end
 end
