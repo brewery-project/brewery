52c4b3fdecd1272941324817b96d85caf9d3cf8d
Nathan Samson <nathan.samson@beeple.eu>
2018-07-06 16:07:20 +0200

    Fixes for archiving

--- app/models/brewery/auth_core/user.rb
+++ app/models/brewery/auth_core/user.rb
@@ -7,8 +7,8 @@ module Brewery
       config.logged_in_timeout = 1.hours
       config.disable_perishable_token_maintenance = true
       config.crypto_provider = Authlogic::CryptoProviders::Sha512
-      config.validates_uniqueness_of_email_field_options(scope: :tenant_id)
-      config.validates_length_of_password_field_options = { minimum: 4, if: :require_password? }
+      config.validates_uniqueness_of_email_field_options(scope: :tenant_id, conditions: -> { where(archived_at: nil) } )
+      config.validate_password_field = false
     end
 
     before_create do
